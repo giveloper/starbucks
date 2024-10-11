0d6d5500bac1acf5bcc8765b05735d2ba6712f38
giveloper <giveloperofficial@gmail.com>
2024-10-11 18:26:29 +0900

    NOTICE PROMOTION 영역 Swiper 라이브러리 적용

--- js/main.js
+++ js/main.js
@@ -53,3 +53,13 @@ new Swiper('.notice-line .swiper', {
   autoplay: true,
   loop: true
 });
+
+new Swiper('.promotion .swiper', {
+  slidesPerView: 3,  // 한번에 보여줄 슬라이드 개수
+	spaceBetween: 10,  // 슬라이드 사이 여백
+	centeredSlides: true,  // 1번 슬라이드가 가운데 보이기 
+	loop: true,
+	autoplay: {
+		delay: 5000
+	}
+});
