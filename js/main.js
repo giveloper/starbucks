7f99f77c29e70dfcc134664438073b6d0a7ee9fc
giveloper <giveloperofficial@gmail.com>
2024-10-12 17:09:07 +0900

    AWARDS 영역 개발

--- js/main.js
+++ js/main.js
@@ -72,6 +72,17 @@ new Swiper('.promotion .swiper', {
   }
 });
 
+new Swiper('.awards .swiper', {
+	autoplay: true,
+	loop: true,
+	spaceBetween: 30,
+	slidesPerView: 5,
+	navigation: {
+		prevEl: '.awards .swiper-custom-button-prev',
+		nextEl: '.awards .swiper-custom-button-next'
+	}
+});
+
 const promotionEl = document.querySelector('.promotion');
 const promotionToggleBtn = document.querySelector('.toggle-promotion');
 let isHidePromotion = false;
